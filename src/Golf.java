f4a5276a9a60ebd9d863fa5091cebadeb2a1b560
sanghpriya1 <sanghpriya2009@gmail.com>
2021-05-14 01:51:25 +0530

    Second Commit

---
+++ src/Golf.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Golf {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0) {
+			int N =  sc.nextInt();
+			int x =  sc.nextInt();
+			int k =  sc.nextInt();
+			int sum = N + x + k;
+			System.out.println(sum);
+		}
+		sc.close();
+	}
+}
