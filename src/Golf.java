fe3cccb790c286a40da8aad597997eebb9444ba0
sanghpriya1 <sanghpriya2009@gmail.com>
2021-05-14 19:02:33 +0530

    Second Problem

--- src/Golf.java
+++ src/Golf.java
@@ -4,12 +4,16 @@ public class Golf {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		while(t-- > 0) {
-			int N =  sc.nextInt();
-			int x =  sc.nextInt();
-			int k =  sc.nextInt();
-			int sum = (N + x + k)*0;
-			System.out.println(sum);
+		while (t-- > 0) {
+			int N = sc.nextInt();
+			int x = sc.nextInt();
+			int k = sc.nextInt();
+			int reverse = N+1 - x;
+			if (x % k == 0 || reverse%k == 0)
+				System.out.println("Yes");
+			else
+				System.out.println("No");
+
 		}
 		sc.close();
 	}
