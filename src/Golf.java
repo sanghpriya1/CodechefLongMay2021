004beeb3eb222f462eefe19bdd855f631c6f3511
sanghpriya1 <sanghpriya2009@gmail.com>
2021-05-14 19:08:10 +0530

    Second Problem

--- src/Golf.java
+++ src/Golf.java
@@ -8,11 +8,13 @@ public class Golf {
 			int N = sc.nextInt();
 			int x = sc.nextInt();
 			int k = sc.nextInt();
-			int reverse = N+1 - x;
-			if (x % k == 0 || reverse%k == 0)
-				System.out.println("Yes");
+			int reverse = N - x;
+			if ((x % k == 0))
+				System.out.println("YES");
+			else if ((reverse + 1) % k == 0)
+				System.out.println("YES");
 			else
-				System.out.println("No");
+				System.out.println("NO");
 
 		}
 		sc.close();
